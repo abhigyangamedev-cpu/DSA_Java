832018b602310ed7b05cf9e5fba14f0abf0de7ec
abhigyandey94 <abhigyangamedev@gmail.com>
2026-09-05 06:24:22 +0530

    ReverseLinkedList done

---
+++ DSA_Java/src/LinkedList/ReverseLinkedList.java
@@ -0,0 +1,44 @@
+package LinkedList;
+
+public class ReverseLinkedList {
+    public static ListNode reverseList(ListNode head) {
+        if(head == null || head.next == null){
+            return head;
+        }
+
+        ListNode prev = null;
+        ListNode curr = head;
+
+        while(curr != null){
+            ListNode temp = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = temp;
+        }
+
+        return prev;
+    }
+
+    public static void main(String[] args){
+        ListNode one = new ListNode(1);
+        ListNode two = new ListNode(2);
+        ListNode three = new ListNode(3);
+        ListNode four = new ListNode(4);
+        ListNode five = new ListNode(5);
+
+        one.next = two;
+        two.next = three;
+        three.next = four;
+        four.next = five;
+
+        System.out.println("Printing the linked list");
+        ListNode.printList(one);
+
+        ListNode ans = reverseList(one);
+
+        System.out.println("Printing the reversed linked list");
+        ListNode.printList(ans);
+
+
+    }
+}
