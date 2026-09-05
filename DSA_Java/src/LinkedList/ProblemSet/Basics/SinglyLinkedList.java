07bea70a6e1cf53cbb5b5f381e85758845d3ec1d
abhigyandey94 <abhigyangamedev@gmail.com>
2026-09-05 20:23:01 +0530

    Added FIrst Singly Linked List

--- DSA_Java/src/LinkedList/ProblemSet/Basics/SinglyLinkedList.java
+++ DSA_Java/src/LinkedList/ProblemSet/Basics/SinglyLinkedList.java
@@ -1,7 +1,5 @@
 package LinkedList.ProblemSet.Basics;
 
-import LinkedList.ListNode;
-
 public class SinglyLinkedList {
     int val;
     SinglyLinkedList next;
@@ -51,6 +49,12 @@ public class SinglyLinkedList {
         return head;
     }
     private static SinglyLinkedList InsertAtPosition(SinglyLinkedList node, int position){
+        if(position == 1){
+            node.next = head;
+            head = node;
+            return head;
+        }
+
         SinglyLinkedList curr = head;
         int counter = 1;
 
@@ -66,11 +70,93 @@ public class SinglyLinkedList {
 
         return head;
     }
-    private static SinglyLinkedList DeleteAtStart(){
-        if(head == null || head.next == null) return null;
+    private static SinglyLinkedList DeleteAtStart() {
+        if (head == null) return null;
         head = head.next;
         return head;
     }
+    private static SinglyLinkedList DeleteAtEnd(){
+        if(head == null) return null;
+
+        if(head.next == null){
+            head = null;
+            return head;
+        }
+
+        SinglyLinkedList curr = head;
+
+        while(curr.next.next != null){
+            curr = curr.next;
+        }
+
+        curr.next = null;
+        return head;
+    }
+    private static SinglyLinkedList DeleteAtPosition(int position){
+        if(head == null) return null;
+
+        if(position == 1){
+            head = head.next;
+            return head;
+        }
+
+        SinglyLinkedList curr = head;
+        int counter = 1;
+
+        while(curr != null && counter < position - 1){
+            curr = curr.next;
+            counter++;
+        }
+
+        if(curr != null && curr.next != null){
+            curr.next = curr.next.next;
+        }else{
+            System.out.println("Position Not Found");
+        }
+
+        return head;
+    }
+    private static boolean CheckIfPresent(int value){
+        SinglyLinkedList curr = head;
+
+        while(curr != null){
+            if(curr.val == value){
+                return true;
+            }
+            curr = curr.next;
+        }
+
+        return false;
+    }
+    private static SinglyLinkedList UpdateElement(int oldValue, int newValue) {
+        SinglyLinkedList curr = head;
+
+        while (curr != null) {
+            if (curr.val == oldValue) {
+                curr.val = newValue;
+                break;
+            }
+            curr = curr.next;
+        }
+
+        return head;
+    }
+    private static SinglyLinkedList ReverseList(SinglyLinkedList list){
+        if(list == null || list.next == null) return list;
+
+        SinglyLinkedList prev = null;
+        SinglyLinkedList curr = list;
+
+        while (curr != null){
+            SinglyLinkedList temp = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = temp;
+        }
+
+        return prev;
+    }
+
 
     public static void main(String[] args){
         SinglyLinkedList one = new SinglyLinkedList(1);
@@ -105,5 +191,26 @@ public class SinglyLinkedList {
         System.out.println("Deleting at Start");
         DeleteAtStart();
         Traversal(head);
+
+        System.out.println("Deleting at Start");
+        DeleteAtEnd();
+        Traversal(head);
+
+        System.out.println("Deleting at position 2");
+        DeleteAtPosition(2);
+        Traversal(head);
+
+        System.out.println("Checking if 6 is presend or not :- "+CheckIfPresent(6));
+
+        System.out.println("Checkig if 5 is present or :- " + CheckIfPresent(5));
+
+        System.out.println("Updating node 3 value with 5");
+        UpdateElement(3,5);
+        Traversal(head);
+
+        System.out.println("Printing the reverse list");
+        SinglyLinkedList ReversedList = ReverseList(head);
+        Traversal(ReversedList);
+
     }
 }
