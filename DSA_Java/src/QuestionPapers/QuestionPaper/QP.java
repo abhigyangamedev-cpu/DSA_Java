cf68dc17c5726885534534ddf5c0e5a267e781dc
abhigyangamedev-cpu <abhigyangamedev@gmail.com>
2026-09-10 16:13:33 +0000

    Initilized a proper QB

---
+++ DSA_Java/src/QuestionPapers/QuestionPaper/QP.java
@@ -0,0 +1,18 @@
+package QuestionPapers.QuestionPaper;
+
+public class QP {
+    class ListNode{
+        int val;
+        ListNode next;
+        ListNode head;
+
+        ListNode(){}
+
+        ListNode(int val){this.val = val;}
+
+        ListNode(int val, ListNode next){
+            this.val = val;
+            this.next = next;
+        }
+    }
+}
